b7af1749f832adddf2af007fe561ed51fa7faa03
cpinom11 <carlospinom1999@gmail.com>
2026-06-06 21:44:44 -0400

    avance de visualización de salas

--- frontend/src/app/features/salas/salas/salas.ts
+++ frontend/src/app/features/salas/salas/salas.ts
@@ -1,9 +1,14 @@
+import { NavbarComponent } from '../../../core/components/navbar/navbar';
+import { FooterComponent } from '../../../core/components/footer/footer';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-salas',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    FooterComponent
+  ],
   templateUrl: './salas.html',
   styleUrl: './salas.css',
 })
