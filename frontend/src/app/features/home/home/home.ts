e7917eac814d96148ec7fe5213b2ec63af1b66dd
cpinom11 <carlospinom1999@gmail.com>
2026-06-08 01:22:27 -0400

    mejoras visuales en el frontend

--- frontend/src/app/features/home/home/home.ts
+++ frontend/src/app/features/home/home/home.ts
@@ -1,11 +1,12 @@
 import { NavbarComponent } from '../../../core/components/navbar/navbar';
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../../core/components/footer/footer";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
